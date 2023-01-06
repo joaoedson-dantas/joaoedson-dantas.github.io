16817ac44f459dc2040d0b4c0a38855c9d828430
João Edson <joaoedson.dantas@outlook.com>
2023-01-06 12:09:44 -0300

    Atualização do portifólio adionando javascript e accordionList

--- script/script.js
+++ script/script.js
@@ -1,11 +1,53 @@
-const btnMobile = document.getElementById("btn-mobile");
-
-function toggleMenu(event) {
-  if (event.type === "touchstart") event.preventDefaut();
-  const nav = document.getElementById("nav");
-  nav.classList.toggle("active");
-  event.currentTarget.setAttribute("aria-expanded", "true");
+// Ativando o js
+function ativoJs() {
+  const body = document.querySelector("body");
+  body.classList.add("js");
 }
+ativoJs();
 
-btnMobile.addEventListener("click", toggleMenu);
-btnMobile.addEventListener("touchstart", toggleMenu);
+// BTN MOBILE
+function initMenuMobile() {
+  const btnMobile = document.getElementById("btn-mobile");
+  const nav = document.getElementById("nav");
+  const Linksinternos = document.querySelectorAll('[href^="#"]');
+
+  function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventDefaut();
+    nav.classList.toggle("active");
+    event.currentTarget.setAttribute("aria-expanded", "true");
+  }
+
+  function clickLink() {
+    nav.classList.remove("active");
+  }
+
+  Linksinternos.forEach((item) => {
+    item.addEventListener("click", clickLink);
+  });
+
+  btnMobile.addEventListener("click", toggleMenu);
+  btnMobile.addEventListener("touchstart", toggleMenu);
+}
+// End
+initMenuMobile();
+
+// Accordion List
+
+function initiAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion h3");
+  const ativoClass = "ativo";
+  accordionList[0].classList.add(ativoClass);
+  accordionList[0].nextElementSibling.classList.add(ativoClass);
+  if (accordionList.length) {
+    function activeAccordion(event) {
+      this.classList.toggle(ativoClass);
+      const itemClicado = event.target;
+      itemClicado.nextElementSibling.classList.toggle(ativoClass);
+    }
+
+    accordionList.forEach((item) =>
+      item.addEventListener("click", activeAccordion)
+    );
+  }
+}
+initiAccordion();
